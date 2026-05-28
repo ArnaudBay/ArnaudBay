3868dc2e0759702568e14d72d7e3d6093d35d36e
ArnaudBay <arnaudbayale@gmail.com>
2026-05-28 09:52:49 +0100

    ajout de vercel analytique

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import About from "./pages/About";
@@ -19,6 +20,7 @@ const App = () => (
         <Route path="/blog" element={<Blog />} />
       </Route>
     </Routes>
+    <Analytics />
   </BrowserRouter>
 );
 
